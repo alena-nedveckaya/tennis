a3aeb08ea3aeaa17cbcad0324a4df14e642d7157
alena-nedveckaya <alenashcherb@gmail.com>
2018-06-16 15:26:55 +0300

    new project

---
+++ App.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import TennisResults from './components/TennisResults.js'
+
+ReactDOM.render(
+    <TableResults results={}/>,
+    document.getElementsById('tennis_results')
+);
